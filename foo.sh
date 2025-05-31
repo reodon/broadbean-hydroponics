5102dbe647b84a13f3a28c721ab1368b0387d41d
reodon <reodon@@users.noreply.github.com>
2025-05-31 21:42:33 +0900

    Update foo.sh

--- foo.sh
+++ foo.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-s=34
+## @see [macOS 15.4.1 - date… - Apple コミュニティ](https://discussionsjapan.apple.com/thread/256047581?sortBy=rank)
+LANG=C
+
+s=52
+sd=20250412
+
+sut=`date -j -f "%Y%m%d" "$sd" "+%s"`
 
 cd $1
 for i in `find . -type f -name '*.jpg'`
@@ -12,6 +18,7 @@ done
 
 for d in `find . -mindepth 1 -maxdepth 1 -type d | sed 's!^\./!!' | sort -n`;
 do
-  mv $d `printf '%03d' $s`
-  s=$((s+1))
+  dut=`date "-v+${s}d" -j -f "%Y%m%d" $d "+%s"`
+  dd=`printf '%03d' $(((dut - sut) / (60 * 60 * 24)))`
+  mv $d $dd
 done
